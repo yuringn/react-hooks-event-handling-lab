2a82ef8d89e758f96f97bade153c4ecbd91c0ee5
Yuri Nguyen <yuringuyen312@gmail.com>
2021-04-28 22:16:48 -0400

    done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,15 @@
 // Code EyesOnMe Component Here
+import React from "react"
+function EyesOnMe(){
+
+    // function handleClick(){
+    //     console.log("Good!")
+    // }
+    return(
+        <div>
+            <button onFocus={()=>console.log("Good!")} onBlur={(e)=>console.log("Hey! Eyes on me!")}>Eyes on me</button>
+        </div>
+    )
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,18 @@
 // Code Keypad Component Here
+import React from "react";
+
+function Keypad(){
+
+    // function handleType(){
+    //     console.log("Entering password...")
+    // }
+    return(
+        <div>
+            <input
+             type="password"
+             onChange={()=>console.log("Entering password...")} />
+        </div>
+    )
+}
+
+export default Keypad;
